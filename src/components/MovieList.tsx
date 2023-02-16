d0c361f35391e186228806835e9e048a85096a6f
beuleu <billyklotz789@gmail.com>
2023-02-16 14:39:07 +0100

    feat Movie List done

---
+++ src/components/MovieList.tsx
@@ -0,0 +1,16 @@
+import React         from 'react';
+import { MovieCard } from "./MovieCard";
+import { MovieType } from "../models/MovieType";
+import "./MovieList.css";
+
+type MovieListProps = {
+  data: MovieType[]
+}
+
+export const MovieList = ({data}: MovieListProps) => {
+  return (
+      <ul className={"movie-list"}>
+        { data.map((movie: MovieType) => <MovieCard movieData={ movie } key={ movie.id }/>) }
+      </ul>
+  )
+}
