cd10f2830c9bd7cb2d6ffb34c3989dac744697d8
beuleu <billyklotz789@gmail.com>
2023-02-17 12:18:03 +0100

    fix changes for pull request

--- src/components/DetailMovie.tsx
+++ src/components/DetailMovie.tsx
@@ -5,12 +5,9 @@ import {useParams} from 'react-router-dom';
 import { movieData } from "../datas";
 
 export const DetailMovie = () => {
-  // const [searchParams, setSearchParams] = useSearchParams();
-  // const test = searchParams.get("id")
-  // console.log(test);
   let {id} = useParams();
   const oneMovie:MovieType = movieData[parseInt(id!)]
-  
+
   return (
     <div className="detailMovie-container">
       <a className="btn-back-movie"  href='/'>&#8592; Back</a>
@@ -19,13 +16,14 @@ export const DetailMovie = () => {
           <img src={oneMovie.poster_path} alt="movie_poster" />
           <div className="detail-info">
           <h3>{oneMovie.title}</h3>
-          <p>{oneMovie.release_date.toString()}</p>
-          <p>{oneMovie.genre.name}</p>
-          <p>{oneMovie.vote_average}</p>
+          <p>Release Date: <span className="movie-info">{oneMovie.release_date.toString()}</span></p>
+          <p>Genre: <span className="movie-info">{oneMovie.genre.name}</span></p>
+          <p>Consumer Score: <span className="movie-info">{oneMovie.vote_average}</span></p>
           </div>
         </div>
 
-        <hr/>
+        <hr className={"line"} />
+
         <div className="resume-movie">
           <h3>Synopsis</h3>
           <p>{oneMovie.overview}</p>
