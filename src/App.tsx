160de38f7057a0d537d4fd857e389920973e1358
Camille-BAE <camille.jbae@gmail.com>
2023-03-02 11:34:42 +0100

    fetch api key categoryList

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,49 @@
-import React                                   from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Home }         from "./components/Home";
-import { CategoryList, movieData } from "./datas";
+import { Home } from "./components/Home";
+import { movieData } from "./datas";
 import { DetailMovie } from "./components/DetailMovie";
-
+import axios from "axios";
+import { CategoryType } from "./models/CategoryType";
 
 const fetchMovies = (type: string) => {
-  console.log(`fetching ${ type } movies`);
-}
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home categoryList={CategoryList} onclick={fetchMovies} data={movieData} />
-  },
-  {
-   path:'/movie/:id',
-   element: <DetailMovie  />
-   }
-])
+  console.log(`fetching ${type} movies`);
+};
 
 function App() {
+  const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
+  useEffect(() => {
+    axios
+      .get<{ genres: CategoryType[] }>(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`
+      )
+      .then((res) => {
+        setCategoryList(res.data.genres);
+      });
+  }, []);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home
+          categoryList={categoryList}
+          onclick={fetchMovies}
+          data={movieData}
+        />
+      ),
+    },
+    {
+      path: "/movie/:id",
+      element: <DetailMovie />,
+    },
+  ]);
 
   return (
-      <div className="App">
-
-        <RouterProvider router={router}/>
-
-      </div>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -14,3 +14,4 @@ export const MovieList = ({data}: MovieListProps) => {
       </ul>
   )
 }
+
