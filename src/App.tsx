665f5fddb0218af37c7dbecae0742b4d30878d18
Camille-BAE <camille.jbae@gmail.com>
2023-03-09 14:46:58 +0100

    refactor app.tsx and home.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
-import React, { useEffect, useState }                                      from "react";
+import React                                     from "react";
 import "./App.css";
 import { createBrowserRouter, RouterProvider }                             from "react-router-dom";
 import { Home }                                                            from "./pages/Home";
-import { DiscoverList }                                                    from "./datas";
 import { DetailMovie }                                                     from "./pages/DetailMovie";
-import { CategoryType }                                                    from "./types/CategoryType";
-import { MovieType }                                                                          from "./types/MovieType";
-import { getMoviesByCategories, getMoviesByDiscover, getMoviesBySearch, getNowPlayingMovies } from './api/Movie';
-import { getCategories }                                                                      from "./api/Categories";
 
 function App() {
-  const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
-  const [movieList, setMovieList] = useState<MovieType[]>([]);
-
-  const getCategoryList = async () => {
-    const categories = await getCategories()
-    setCategoryList(categories)
-  }
-  const getAllMovies = async () => {
-    const movies = await getNowPlayingMovies()
-    setMovieList(movies);
-  }
-  const getDiscoverMovies = async (discover:string) => {
-    const movies = await getMoviesByDiscover(discover)
-    setMovieList(movies);
-  }
-  const getCategoryMovies = async (category:string) => {
-    const movies = await getMoviesByCategories(category)
-    setMovieList(movies);
-  }
-  const getSearchMovies = async (search:string) => {
-    const movies = await getMoviesBySearch(search)
-    setMovieList(movies)
-  }
-  //useEffect for fetching home page
-  useEffect(() => {
-    getAllMovies();
-    getCategoryList()
-  }, []);
-
-  const fetchDiscoverMovies = (discover: string) => {
-    getDiscoverMovies(discover)
-  };
-  const fetchCategoryMovies = (category: string) => {
-    getCategoryMovies(category)
-  };
-  const fetchSearchedMovies = (search: string) => {
-    getSearchMovies(search)
-  }
-
 
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
-        <Home
-          discoverList={DiscoverList}
-          categoryList={categoryList}
-          onclickDiscover={fetchDiscoverMovies}
-          onclickCategory={fetchCategoryMovies}
-          onSubmitSearch={fetchSearchedMovies}
-          data={movieList}
-        />
+        <Home />
       ),
     },
     {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,40 +1,80 @@
-import React            from "react";
+import React, { useEffect, useState }            from "react";
 import { Navbar }       from "../components/Navbar";
 import { MovieList }    from "../components/MovieList";
 import { CategoryType } from "../types/CategoryType";
 import { MovieType }    from "../types/MovieType";
 import Searchbar        from "../components/Searchbar";
+import { DiscoverList }                                                    from "../datas";
+import { getCategories } from "../api/Categories";
+import { getMoviesByCategories, getMoviesByDiscover, getMoviesBySearch, getNowPlayingMovies } from "../api/Movie";
 
-type HomeProps = {
-  discoverList: string[];
-  categoryList: CategoryType[];
-  onclickDiscover: (arg: string) => void;
-  onclickCategory: (arg: string) => void;
-  onSubmitSearch: (arg: string) => void;
-  data: MovieType[];
-};
 
-export const Home = ({
-  discoverList,
-  categoryList,
-  onclickDiscover,
-  onclickCategory,
-  onSubmitSearch,
-  data
-}: HomeProps) => {
+
+
+
+export const Home = () => {
+  const [categoryList, setCategoryList] = useState<CategoryType[]>([]);
+  const [movieList, setMovieList] = useState<MovieType[]>([]);
+
+  const getAllMovies = async () => {
+    const movies = await getNowPlayingMovies()
+    setMovieList(movies);
+  }
+
+  useEffect(() => {
+    getAllMovies();
+  }, []);
+
+  
+  const getCategoryList = async () => {
+    const categories = await getCategories()
+    setCategoryList(categories)
+  }
+
+  useEffect(() => {
+    getCategoryList()
+  }, []);
+
+  const getCategoryMovies = async (category:string) => {
+    const movies = await getMoviesByCategories(category)
+    setMovieList(movies);
+  }
+
+  const fetchCategoryMovies = (category: string) => {
+    getCategoryMovies(category)
+  };
+  
+  const fetchDiscoverMovies = (discover: string) => {
+    getDiscoverMovies(discover)
+  };
+
+  const getDiscoverMovies = async (discover:string) => {
+    const movies = await getMoviesByDiscover(discover)
+    setMovieList(movies);
+  }
+
+  const getSearchMovies = async (search:string) => {
+    const movies = await getMoviesBySearch(search)
+    setMovieList(movies)
+  }
+
+  const fetchSearchedMovies = (search: string) => {
+    getSearchMovies(search)
+  }
+
   return (
       <div className="Home">
         <header className="App-header">
           <Navbar
-              discoverList={ discoverList }
-              categoryList={ categoryList }
-              onclickDiscover={ onclickDiscover }
-              onclickCategory={ onclickCategory }
+              discoverList={ DiscoverList }
+              categoryList={categoryList}
+              onclickDiscover={ fetchDiscoverMovies }
+              onclickCategory={ fetchCategoryMovies }
           />
         </header>
 
-        <MovieList data={ data }/>
-        <Searchbar onSubmitSearch={onSubmitSearch} />
+        <MovieList data={ movieList }/>
+        <Searchbar onSubmitSearch={fetchSearchedMovies} />
       </div>
   );
 };
